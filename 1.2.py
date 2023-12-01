3bd25e0c6dc18897435645b0bcd5590bac37a229
Henry Meier <32826270+hcm444@users.noreply.github.com>
2023-11-30 23:54:13 -0800

    Create 1.2.py

---
+++ 1.2.py
@@ -0,0 +1,15 @@
+total = 0
+alpha = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+for line in open("input.txt").read().strip().split('\n'):
+    digits = []
+    t_digits = []
+    for i, j in enumerate(line):
+        if j.isdigit():
+            digits.append(j)
+            t_digits.append(j)
+        for k, val in enumerate(alpha):
+            if line[i:].startswith(val):
+                t_digits.append(str(k + 1))
+    if digits:
+        total += int(t_digits[0] + t_digits[-1])
+print(total)
