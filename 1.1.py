b62388744240e1ed6c6804d4c503c54fae5c4e4d
Henry Meier <32826270+hcm444@users.noreply.github.com>
2023-11-30 23:53:48 -0800

    Create 1.1.py

---
+++ 1.1.py
@@ -0,0 +1,7 @@
+total=0
+for i, arr in enumerate([list(line.strip()) for line in (open('input.txt').readlines())], start=1):
+    if [int(j) for j in arr if j.isnumeric()]:
+        result_str = str([int(j) for j in arr if j.isnumeric()][0]) + str([int(j) for j in arr if j.isnumeric()][-1])
+        line = (int(result_str))
+        total += line
+print(total)
