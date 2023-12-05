40c9e42f6aeff1239e85723ce1662f26d508111f
hcm14 <hcm1476@gmail.com>
2023-12-04 16:31:34 -0800

    day 3

---
+++ 3.1.py
@@ -0,0 +1,54 @@
+class Number:
+    def __init__(self):
+        self.value = ''
+        self.coordinates = []
+
+class Symbol:
+    def __init__(self, char, x, y):
+        self.value = char
+        self.x, self.y = x, y
+
+
+if __name__ == "__main__":
+    grid = []
+    with open('input.txt', 'r') as f:
+        for j, line in enumerate(f):
+            row = []
+            current_number = None
+
+            for i, char in enumerate(line.strip()):
+                if char.isdigit():
+                    if not current_number:
+                        current_number = Number()
+                    current_number.value += char
+                    current_number.coordinates.append((i, j))
+                else:
+                    if current_number:
+                        row.append(current_number)
+                        current_number = None
+
+                    if char != '.':
+                        row.append(Symbol(char, i, j))
+
+            if current_number:
+                row.append(current_number)
+
+            grid.append(row)
+    input_grid = grid
+    total = 0
+    for j, row1 in enumerate(input_grid):
+        min_j, max_j = max(0, j - 1), min(len(input_grid), j + 1)
+        adjacent_rows = input_grid[min_j:max_j + 1]
+
+        for item in row1:
+            if isinstance(item, Symbol):
+                si, sj = item.x, item.y
+                total += sum(
+                    int(number.value)
+                    for adjacent_row in adjacent_rows
+                    for number in adjacent_row
+                    if isinstance(number, Number) and any(
+                        abs(i1 - si) <= 1 and abs(j1 - sj) <= 1 for i1, j1 in number.coordinates)
+                )
+    result_part1 = total
+    print(result_part1)
---
+++ 3.2.py
@@ -0,0 +1,80 @@
+class Number:
+    def __init__(self):
+        self.value = ''
+        self.coords = []
+
+    def is_adjacent_to(self, symbol):
+        symbol_x = symbol.x
+        symbol_y = symbol.y
+
+        for x, y in self.coords:
+            if abs(x - symbol_x) <= 1 and abs(y - symbol_y) <= 1:
+                return True
+        return False
+
+
+class Symbol:
+    def __init__(self, character, x, y):
+        self.value = character
+        self.x = x
+        self.y = y
+
+
+def input_parser(file_path='input.txt'):
+    schematic = []
+
+    with open(file_path, 'r') as file:
+        for j, line in enumerate(file):
+            schematic_row = []
+
+            current_number = Number()
+            for i, character in enumerate(line.strip()):
+                if character.isdigit():
+                    current_number.value += character
+                    current_number.coords.append((i, j))
+                else:
+                    if current_number.value:
+                        schematic_row.append(current_number)
+                        current_number = Number()
+
+                    if character != '.':
+                        schematic_row.append(Symbol(character, i, j))
+
+            if current_number.value:
+                schematic_row.append(current_number)
+
+            schematic.append(schematic_row)
+
+    return schematic
+
+
+def part2(schematic):
+    schematic_length = len(schematic)
+    gear_ratio_sum = 0
+
+    for j, row in enumerate(schematic):
+        min_j = max(0, j - 1)
+        max_j = min(schematic_length, j + 1)
+        adjacent_rows = schematic[min_j:max_j + 1]
+
+        for item in row:
+            if item.value == '*':
+                adjacent_parts = []
+
+                for adj_row in adjacent_rows:
+                    for other_item in adj_row:
+                        if isinstance(other_item, Number) and other_item.is_adjacent_to(item):
+                            adjacent_parts.append(other_item)
+
+                if len(adjacent_parts) == 2:
+                    part1, part2 = adjacent_parts
+                    gear_ratio = int(part1.value) * int(part2.value)
+                    gear_ratio_sum += gear_ratio
+
+    return gear_ratio_sum
+
+
+if __name__ == "__main__":
+    schematic = input_parser()
+    result_part2 = part2(schematic)
+    print(result_part2)
